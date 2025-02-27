c5281bc115e0289d27b8bb0de3135a3fd41a3065
ShebalkovD <shebalkovdenis.wk@gmail.com>
2025-02-27 21:40:13 +0300

    Получение данных из формы

--- src/components/CreateForm/CreateForm.jsx
+++ src/components/CreateForm/CreateForm.jsx
@@ -6,28 +6,34 @@ import Button from '../Button/Button'
 export default function CreateForm() {
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log(event.target.elements.answer.value)
+        const data = new FormData(event.target)
+        console.log(data.get('correct')) // Получить по ключу
+
+        // Перебрать все значения
+        // for (const [key, value] of data) {
+        //     console.log(key, value)
+        // }
     }
 
     return(
         <form action="" className={classes.form} onSubmit={handleSubmit}>
-            <Input label="Текст вопроса" />
+            <Input label="Текст вопроса" inputName="question"/>
 
             <div className={classes.form_text}>
                 Введите ответы на вопрос и отметье верный
             </div>
 
             <div className={classes.wrapper}>
-                <RadioInput name="answer" value="1" className={classes.radio} checked="checked" />
-                <Input label="Ответ 1" />
+                <RadioInput name="correct" value="1" className={classes.radio} checked="checked" />
+                <Input label="Ответ 1" inputName="answer1"/>
             </div>
             <div className={classes.wrapper}>
-                <RadioInput name="answer" value="2" className={classes.radio}/>
-                <Input label="Ответ 2" />
+                <RadioInput name="correct" value="2" className={classes.radio}/>
+                <Input label="Ответ 2" inputName="answer2"/>
             </div>
             <div className={classes.wrapper}>
-                <RadioInput name="answer" value="3" className={classes.radio}/>
-                <Input label="Ответ 3" />
+                <RadioInput name="correct" value="3" className={classes.radio}/>
+                <Input label="Ответ 3" inputName="answer2"/>
             </div>
 
             <Button >Подтвердить</Button>
